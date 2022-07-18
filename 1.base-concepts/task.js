0281cbcc34458f173b40ad49c96172704cb7361c
Kate Pro <kate@MacBook-Pro-Ekaterina.local>
2022-07-18 15:44:20 +0300

    first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,20 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  const d = b ** 2 - 4 * a * c;
+
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else if (d > 0) {
+    const dsq = Math.sqrt(d);
+    const x1 = (-b + dsq) / (2 * a);
+    const x2 = (-b - dsq) / (2 * a);
+    arr.push(x1);
+    arr.push(x2);
+  }
+
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
