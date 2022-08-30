97cd208f6e262c5921d02a2592b5cab5ea36e9a8
Kate Pro <kate@MacBook-Pro-Ekaterina.local>
2022-08-31 01:12:05 +0400

    Caching & debounce decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,46 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cache = {};
+  return function (...arg) {
+    const hash = arg.join(', ');
+    let result = cache[hash];
+    if (result === undefined) {
+      if (Object.keys(cache).length === 5) {
+       delete cache[Object.keys(cache)[0]];
+      } 
+      cache[hash] = result = func.call(this, ...arg);
+      console.log('Вычисляем: ' + result);
+      return 'Вычисляем: ' + result; 
+    } else {
+      console.log('Из кэша: ' + result);
+      return 'Из кэша: ' + result;
+    }
+  }
 }
 
+function debounceDecoratorNew(f, ms) {
+  let timerId = null;
+  let wrapper;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+  wrapper = function(...args) {
+    wrapper.allCount++;
 
-function debounceDecorator2(func) {
-  // Ваш код
-}
+    if (timerId === null) {
+      wrapper.count++;
+      f.apply(this, args);
+    } else {
+      // Отменить предыдущий сигнал, если до этого уже был запланирован запуск в setTimeout.
+      clearTimeout(timerId); 
+    }
+
+    // Запланировать следующий запуск, который может быть отменен при повторном вызове.
+    timerId = setTimeout(() => {
+      wrapper.count++;
+      f.apply(this, args); 
+    }, ms);
+  };
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
+};
